b78340961702fc6476126bb84603af2dc10c9bd5
clouderow@gmail.com <clouderow@gmail.com>
2022-04-22 13:13:08 +0000

    question 2

---
+++ pygcn/run.bash
@@ -0,0 +1,8 @@
+python train.py --num_samples=50
+python train.py --num_samples=100
+python train.py --num_samples=200
+python train.py --num_samples=500
+python train.py --num_samples=1000
+python train.py --num_samples=1500
+python train.py --num_samples=2000
+python train.py --num_samples=2708
--- pygcn/train.py
+++ pygcn/train.py
@@ -21,6 +21,8 @@ parser.add_argument('--fastmode', action='store_true', default=False,
 parser.add_argument('--seed', type=int, default=42, help='Random seed.')
 parser.add_argument('--epochs', type=int, default=200,
                     help='Number of epochs to train.')
+parser.add_argument('--num_samples', type=int, default=None,
+                    help='Number of samples in a batch.')
 parser.add_argument('--lr', type=float, default=0.01,
                     help='Initial learning rate.')
 parser.add_argument('--weight_decay', type=float, default=5e-4,
@@ -38,8 +40,11 @@ torch.manual_seed(args.seed)
 if args.cuda:
     torch.cuda.manual_seed(args.seed)
 
-# Load data
+# Load data: https://relational.fit.cvut.cz/dataset/CORA
 adj, features, labels, idx_train, idx_val, idx_test = load_data()
+
+# adj: [2708, 2708]
+# features: [2708, 1433]
 
 # Model and optimizer
 model = GCN(nfeat=features.shape[1],
@@ -48,7 +53,7 @@ model = GCN(nfeat=features.shape[1],
             dropout=args.dropout)
 optimizer = optim.Adam(model.parameters(),
                        lr=args.lr, weight_decay=args.weight_decay)
-
+#%%
 if args.cuda:
     model.cuda()
     features = features.cuda()
@@ -58,16 +63,25 @@ if args.cuda:
     idx_val = idx_val.cuda()
     idx_test = idx_test.cuda()
 
-
-def train(epoch):
+adj_dense = adj.to_dense()
+def train(epoch, num_samples = None):
+    if num_samples is None:
+        num_samples = features.size(0)
     t = time.time()
     model.train()
-    optimizer.zero_grad()
-    output = model(features, adj)
-    loss_train = F.nll_loss(output[idx_train], labels[idx_train])
-    acc_train = accuracy(output[idx_train], labels[idx_train])
-    loss_train.backward()
-    optimizer.step()
+    s = features.size(0)/num_samples
+    for _ in range(features.size(0)//num_samples):
+        # get a mini-batch and select the training samples
+        idx_sample = torch.randperm(features.size(0),device=features.device)[:num_samples]
+        selected_train = (idx_sample.reshape(-1,1) == idx_sample).any(1)
+        labels1 = labels[idx_sample]
+        adj1 = adj_dense[idx_sample][:,idx_sample].to_sparse()
+        optimizer.zero_grad()
+        output = model(features[idx_sample], adj1,s=s)
+        loss_train = F.nll_loss(output[selected_train], labels1[selected_train])*s
+        acc_train = accuracy(output[selected_train], labels1[selected_train])
+        loss_train.backward()
+        optimizer.step()
 
     if not args.fastmode:
         # Evaluate validation set performance separately,
@@ -93,14 +107,26 @@ def test():
     print("Test set results:",
           "loss= {:.4f}".format(loss_test.item()),
           "accuracy= {:.4f}".format(acc_test.item()))
+    res = args.__dict__.copy()
+    res.update({
+        'loss':loss_test.item(),
+        'acc': acc_test.item()
+    })
+    
+    with open('results.txt','a+') as f:
+        f.write(str(res))
+        f.write('\n')
+
 
 
 # Train model
 t_total = time.time()
 for epoch in range(args.epochs):
-    train(epoch)
+    train(epoch,args.num_samples)
 print("Optimization Finished!")
 print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
 
 # Testing
 test()
+# Epoch: 0200 loss_train: 0.4095 acc_train: 0.9286 loss_val: 0.6867 acc_val: 0.8167 time: 0.0093s
+# Test set results: loss= 0.7265 accuracy= 0.8300
